2afc095e86b36e08c2797e515a62e7fa6325fbe5
root <root@886008c73cea>
2020-06-11 02:59:13 +0000

    I learned how to make an input and draw things with the Robot.

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,9 +8,14 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String input= JOptionPane.showInputDialog("Do you know how to write code?") ;
 		// 2. If they say "yes", tell them they will rule the world.
-
+if (input.equals("yes")) {
+	System.out.println("You will rule the world!");
+}
+else {
+	System.out.println("Sign up for classes at The League");
+}
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
 	}
